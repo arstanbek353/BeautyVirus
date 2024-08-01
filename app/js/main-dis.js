544e7553cb5e35f8667a64405d7de10a1aecceb9
arstanbek353 <arstanbek353@gmail.com>
2024-08-01 18:05:44 +0600

    new pages

--- app/js/main-dis.js
+++ app/js/main-dis.js
@@ -21,4 +21,50 @@ export default function () {
       },
     });
   })
+
+  const sectionNodes2 = document.querySelectorAll('.main-dis2__left')
+  sectionNodes2.forEach(element => {
+    const sliderNode = element.querySelector('.main-dis2__slider')
+    const pagNode = element.querySelector('.main-dis2__pag')
+    const prevNode = element.querySelector('.main-dis2__prev')
+    const nextNode = element.querySelector('.main-dis2__next')
+    const delay = 3000
+    var swiper = new Swiper(sliderNode, {
+      slidesPerView: 1,
+      pagination: {
+        el: pagNode,
+        type: 'bullets',
+        bulletClass: 'bullet',
+        bulletActiveClass: 'active',
+      },
+      navigation: {
+        nextEl: nextNode,
+        prevEl: prevNode,
+        disabledClass: 'disabled',
+      },
+    });
+  })
+
+  const sectionNodes3 = document.querySelectorAll('.main-dis2__right')
+  sectionNodes3.forEach(element => {
+    const sliderNode = element.querySelector('.main-dis2__slider')
+    const pagNode = element.querySelector('.main-dis2__pag')
+    const prevNode = element.querySelector('.main-dis2__prev')
+    const nextNode = element.querySelector('.main-dis2__next')
+    const delay = 3000
+    var swiper = new Swiper(sliderNode, {
+      slidesPerView: 1,
+      pagination: {
+        el: pagNode,
+        type: 'bullets',
+        bulletClass: 'bullet',
+        bulletActiveClass: 'active',
+      },
+      navigation: {
+        nextEl: nextNode,
+        prevEl: prevNode,
+        disabledClass: 'disabled',
+      },
+    });
+  })
 }
--- app/js/main-prods.js
+++ app/js/main-prods.js
@@ -15,4 +15,20 @@ export default function () {
       },
     });
   })
+  const sectionNodes2 = document.querySelectorAll('.main-prods-num')
+  sectionNodes2.forEach(element => {
+    const sliderNode = element.querySelector('.main-prods-num__slider')
+    const pagNode = element.querySelector('.main-prods-num__pag')
+    const prevNode = element.querySelector('.main-prods-num__prev')
+    const nextNode = element.querySelector('.main-prods-num__next')
+    const delay = 3000
+    var swiper = new Swiper(sliderNode, {
+      slidesPerView: 'auto',
+      navigation: {
+        nextEl: nextNode,
+        prevEl: prevNode,
+        disabledClass: 'disabled',
+      },
+    });
+  })
 }
